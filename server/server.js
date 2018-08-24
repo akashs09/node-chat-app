5af6ba419097662a61a6745b096110324db59d29
Akash Sharma <sharma.akash028@gmail.com>
2018-08-24 19:35:49 -0400

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 //needs to serve up the public directory
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express(); //app will create routes, run middleware and listen
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 
+
+const app = express(); //app will create routes, run middleware and listen
+const server = http.createServer(app);
+const io = socketIO(server); //returns websocket server
+
+io.on('connection', (socket) => { //socket refers to the individual
+  console.log('New user connected.'); //register for connected client to server
+
+socket.on('disconnect', () =>{
+  console.log('client disconnected');
+});
+});
 app.use(express.static(publicPath)); //configure our express static middleware
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`node-chat-app serving up port ${port}`);
 });
