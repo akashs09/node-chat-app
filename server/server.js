4a1cf717e57a6db856202e15215efe9294a9b951
Akash Sharma <sharma.akash028@gmail.com>
2018-08-24 18:02:43 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+//needs to serve up the public directory
+const path = require('path');
+const express = require('express');
+const app = express(); //app will create routes, run middleware and listen
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath)); //configure our express static middleware
+
+app.listen(3000, () => {
+  console.log(`node-chat-app serving up port ${port}`);
+});
