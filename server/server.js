d06ef6d4931fc588566c404473cda26eeb0b3de9
Akash Sharma <sharma.akash028@gmail.com>
2018-08-30 18:31:18 -0400

    Add newMEssage and createMessage events

--- server/server.js
+++ server/server.js
@@ -12,15 +12,27 @@ const publicPath = path.join(__dirname, '../public');
 const app = express(); //app will create routes, run middleware and listen
 const server = http.createServer(app);
 const io = socketIO(server); //returns websocket server
+app.use(express.static(publicPath)); //configure our express static middleware
 
 io.on('connection', (socket) => { //socket refers to the individual
   console.log('New user connected.'); //register for connected client to server
 
-socket.on('disconnect', () =>{
-  console.log('client disconnected');
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hey. What is going on.',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (newMessage)=>{
+    console.log('createEmail', newMessage);
+  });
+
+  socket.on('disconnect', () =>{
+    console.log('client disconnected');
 });
 });
-app.use(express.static(publicPath)); //configure our express static middleware
+
+
 
 server.listen(port, () => {
   console.log(`node-chat-app serving up port ${port}`);
